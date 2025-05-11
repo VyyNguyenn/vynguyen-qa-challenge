b9a06529d0413fd300f0683c26bf09fc25c1dc38
VyyNguyenn <vytn101@gmail.com>
2025-05-12 03:39:33 +0700

    updated script

--- src/helpers/TestHelpers.ts
+++ src/helpers/TestHelpers.ts
@@ -6,10 +6,18 @@ export default class TestHelpers extends BasePage {
   constructor(page: Page) {
     super(page);
   }
-
-  async loginSuccessfully(page: Page, userName: string, password: string) {
-    const loginPage = new LoginPage(page);
-    await loginPage.navigateToLoginPage();
-    await loginPage.login(userName, password);
+  
+  async loginSuccessfully(userName: string, password: string): Promise<boolean> {
+    try {
+      const loginPage = new LoginPage(this.page);
+      await loginPage.navigateToLoginPage();
+      await loginPage.login(userName, password);
+      await loginPage.verifySuccessfulLogin();
+      console.log("Login successful.");
+      return true;
+    } catch (error) {
+      console.error("Login failed:", error);
+      return false;
+    }
   }
 }
--- src/tests/admin.spec.ts
+++ src/tests/admin.spec.ts
@@ -1,6 +1,7 @@
 import test, { expect } from "@playwright/test";
 import TestHelpers from "../helpers/TestHelpers";
 import AdminPage from "../pages/AdminPage";
+import { VALID_USERNAME } from "../utils/constants";
 
 const SEARCH_USERNEAME_KEYWORD = 'Admin';
 const SEARCH_EMPLOYEENAME_KEYWORD = 'test';
@@ -11,22 +12,22 @@ test.describe('Search Tests in Admin Listing page', () => {
     test.beforeEach(async ({ page }) => {
         const testHelpers = new TestHelpers(page);
         adminPage = new AdminPage(page);
-        await testHelpers.loginSuccessfully(page, 'Admin', 'admin123');
+        await testHelpers.loginSuccessfully(VALID_USERNAME, VALID_USERNAME);
         await adminPage.navigateToAdminPage();
     });
 
-    test('SE-01: Search for Username using valid keyword', async ({ page }) => {
+    test('SE-01: Search for Username using valid keyword', async () => {
         await adminPage.searchByUserName(SEARCH_USERNEAME_KEYWORD);
         expect(adminPage.getUserNameResult(SEARCH_USERNEAME_KEYWORD)).toBeTruthy();
     });
 
-    test('SE-02: Search for Employee Name using valid keyword', async ({ page }) => {
+    test('SE-02: Search for Employee Name using valid keyword', async () => {
         await adminPage.searchByEmployeeName(SEARCH_EMPLOYEENAME_KEYWORD);
         const allSuggestionsContainName = await adminPage.getEmployeeNameSuggestions(SEARCH_EMPLOYEENAME_KEYWORD);
         expect(allSuggestionsContainName).toBeTruthy();
     });
 
-    test('SE-03: Search for no result Username', async ({ page }) => {
+    test('SE-03: Search for no result Username', async () => {
         await adminPage.searchByUserName('nonexistentuser');
         await expect(adminPage.noResultsFoundText).toBeVisible();
     });
